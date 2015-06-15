15f5aec4400c3209d40123a96923e8b0a2db2cec
Jake <jake@jakebladt.com>
2015-06-15 17:49:27 -0400

    +line

---
+++ fractalline/FractalLineMaker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fractalline
+{
+    public static class FractalLineMaker
+    {
+        public LinkedList<CartesianPoint> MakeFractalLine(CartesianPoint start, CartesianPoint end, double naxStepLength, double smoothness)
+        {
+            var ret = new LinkedList<CartesianPoint>(new List<CartesianPoint> { start });
+            smoothness = Math.Max(1.0, Math.Min(0.0, smoothness));
+
+            var currentPoint = start;
+            
+
+            return ret;
+        }
+    }
+}
