79a738b76886faeced5b74c2b7607fdd1100c3d7
Jake Bladt <jake@jakebladt.com>
2016-10-28 14:47:20 -0400

    use canvas context

--- fractal.js
+++ fractal.js
@@ -1,19 +1,25 @@
-var fractal = (function($, canvasContext) {
-  var ret = function() {};
-  
-  ret.drawLine = function(x1, y1, x2, y2) {
-    var context = canvasContext.getDrawingContext();
-    context.lineWidth = 2;
-    context.strokeStyle = 'black';
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.stroke();    
+var fractal = (function($) {
+  return function() {
+    var _cc;
+
+    canvasContext: function(cc) {
+      if(cc) _cc = cc;
+      return _cc;
+    },
+
+    drawLine: function(x1, y1, x2, y2) {
+      var context = _cc.getDrawingContext();
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
+      context.moveTo(x1, y1);
+      context.lineTo(x2, y2);
+      context.stroke();    
   }
 
   return ret;
-})(jQuery, jQuery('#mapCanvas')[0].getContext('2d'));
+})(jQuery);
 
 $(document).ready(function() {
-  console.log('jQuery is loaded.');
+  fractal.canvasContext(jQuery('#mapCanvas')[0].getContext('2d'));
   fractal.drawLine(100, 100, 300, 100);
 });
