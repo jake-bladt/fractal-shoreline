d1730cf84cb369c32ca7e953be0c7fe6bec151f4
Jake Bladt <jake@jakebladt.com>
2016-10-28 15:35:59 -0400

    +draw multiline

--- fractal.js
+++ fractal.js
@@ -14,12 +14,22 @@ var fractal = (function($) {
       _cc.moveTo(x1, y1);
       _cc.lineTo(x2, y2);
       _cc.stroke();
-    }
+    },
+
+    drawMultiline: function(points) {
+      for(i = 1; i < points.length; i++) {
+        var start = points[i -1];
+        var end = points[i];
+        drawLine(start.x, start.y, end.x, end.y)
+      }
+    };
   };
 
 })(jQuery);
 
 $(document).ready(function() {
   fractal.canvasContext(jQuery('#mapCanvas')[0].getContext('2d'));
-  fractal.drawLine(100, 100, 300, 100);
+  fractal.drawMultiline([
+    {x: 100, y: 100}, {x: 300, y: 100}, {x: 300, y: 300 }, {x: 100, y: 300}, {x: 100, y: 100}
+  ]);
 });
