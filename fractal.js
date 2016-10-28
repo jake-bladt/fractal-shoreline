1fb98369a65e25da2fc953b7e13ec13658e8f8e9
Jake Bladt <jake@jakebladt.com>
2016-10-28 15:13:41 -0400

    Use 2d context

--- fractal.js
+++ fractal.js
@@ -9,12 +9,11 @@ var fractal = (function($) {
     },
 
     drawLine: function(x1, y1, x2, y2) {
-      var context = _cc.getDrawingContext();
-      context.lineWidth = 2;
-      context.strokeStyle = 'black';
-      context.moveTo(x1, y1);
-      context.lineTo(x2, y2);
-      context.stroke();
+      _cc.lineWidth = 2;
+      _cc.strokeStyle = 'black';
+      _cc.moveTo(x1, y1);
+      _cc.lineTo(x2, y2);
+      _cc.stroke();
     }
   };
 
