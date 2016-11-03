781b55fea9b12b01a8af9f9806b3e1b22f90ddaf
Jake Bladt <jake@jakebladt.com>
2016-11-02 20:40:32 -0400

    call with square

--- fractal.js
+++ fractal.js
@@ -1,6 +1,16 @@
 var fractal = (function($) {
   var _cc = undefined;
 
+  var pointOnLine = function(line, pctDistance, coeff) {
+    var coeffNum = (typeof(coeff) === 'function') ? coeff() : coeff;
+    var deltaX = line.start.x - line.end.x;
+    var deltaY = line.start.y - line.end.y;
+    return {
+      x: deltaX * pctDistance * coeffNum,
+      y: deltaY * pctDistance * coeffNum
+    }
+  };
+
   return {
     canvasContext: function(cc) {
     if(cc) _cc = cc;
@@ -28,22 +38,39 @@ var fractal = (function($) {
         points: [];
       };
 
+      var aroundOne = function(num) { reurn 0.8 + Math.random() * 0.4 };
+
       for(var i = 0; i < shape.points.length - 1; ++i) {
         // when snowflakes live long enough, they evolve into glaciers
         var start = shape.points[i];
-        var end = shape.points[i + 1];
+        var   end = shape.points[i + 1];
         shoreline.points.push(start);
-        // push a point 1/3ish from start to end.
+        shoreline.points.push({start: start, end: end}, 0.333, aroundOne);
         // push a point up to 20% the line's length above
         // or below the line, perpendicular
-        // push a point 2/3ish from start to end.
+        shoreline.points.push({start: start, end: end}, 0.667, aroundOne);
       }; 
-      return newShoreline;
+      return shoreline;
     }
   };
 
 })(jQuery);
 
 $(document).ready(function() {
-  fractal.canvasContext(jQuery('#mapCanvas')[0].getContext('2d'));
+    fractal.canvasContext(jQuery('#mapCanvas')[0].getContext('2d'));
+
+    var shoreline = fractal.generateShoreline(
+    {
+      points: [
+        { x: 100, y:100 },
+        { x: 100, y:600 },
+        { x: 600, y:600 },
+        { x: 600, y:100 },
+        { x: 100, y:100 }
+      ]
+    }
+  );
+
+  fractal.drawMultiline(shoreline);
+
 });
