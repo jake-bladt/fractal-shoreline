e243e265072c68a7212867150c8d12c657287b45
Jake Bladt <jake@jakebladt.com>
2016-11-12 00:47:41 -0500

    allow different colored lines

--- fractal.js
+++ fractal.js
@@ -114,9 +114,9 @@ var fractal = (function($) {
     return _cc;
     },
 
-    drawLine: function(x1, y1, x2, y2) {
+    drawLine: function(x1, y1, x2, y2, color) {
       _cc.lineWidth = 2;
-      _cc.strokeStyle = 'black';
+      _cc.strokeStyle = color || 'black';
       _cc.moveTo(x1, y1);
       _cc.lineTo(x2, y2);
       _cc.stroke();
